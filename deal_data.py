e1a5d7154bdb85f8fb03a269b6e0d5f14ad6c530
JiJiBo <38462693+JiJiBo@users.noreply.github.com>
2024-07-21 18:09:30 +0800

    deal_data.py

--- deal_data.py
+++ deal_data.py
@@ -1,7 +1,7 @@
 import json
 
-# path = r"C:\Users\Nas\Downloads\百科类问答json版\baike_qa_valid.json"
-path = r"C:\Users\Nas\Downloads\百科类问答json版\baike_qa_train.json"
+path = r"C:\Users\Nas\Downloads\百科类问答json版\baike_qa_valid.json"
+# path = r"C:\Users\Nas\Downloads\百科类问答json版\baike_qa_train.json"
 # 读取文件
 result = []
 with open(path, 'r', encoding='utf8') as f:
@@ -11,11 +11,11 @@ with open(path, 'r', encoding='utf8') as f:
         title = data["title"]
         answer = data["answer"]
         if len(answer) > 0 and len(title) > 0:
-            user = {"role": "user", "content": answer}
-            assistant = {"role": "assistant", "content": title}
+            user = {"role": "user", "content": title}
+            assistant = {"role": "assistant", "content": answer}
             result.append(user)
             result.append(assistant)
 end_data = {"messages": result}
 # print(end_data)
-with open(r"C:\Users\Nas\Downloads\百科类问答json版\baike_qa_train_deal.json", 'w', encoding='utf8') as f:
+with open(r"C:\Users\Nas\Downloads\百科类问答json版\baike_qa_valid_deal.json", 'w', encoding='utf8') as f:
     f.write(json.dumps(end_data, ensure_ascii=False, indent=4))
